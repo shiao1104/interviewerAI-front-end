52ef6e095ae475e4c8f1450f02b93692b7008502
hsiao <10856003@ntub.edu.tw>
2025-05-03 23:13:40 +0800

    新增 - manage layout

---
+++ components/Layout/manage/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+import styles from "@/styles/components/layout/Layout.module.scss";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Head>
+        <title>AI面試官</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="NTUB IMD BIRC CMS" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.layout}>
+        <Navbar />
+        <article className={styles.chill}>{children}</article>
+      </main>
+    </>
+  );
+}
