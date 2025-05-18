67857d60d44ac56533881bb1b5012e8582e723cd
hsiao <10856003@ntub.edu.tw>
2025-05-18 23:25:15 +0800

    login api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,23 +5,29 @@ import { SessionProvider } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { usePathname } from "next/navigation";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   useEffect(() => {
-    if (!sessionStorage.getItem("google_token") && pathname !== '/login') {
-      router.push('/login')
+    if (
+      (!sessionStorage.getItem("google_token") ||
+        !sessionStorage.getItem("token")) &&
+      pathname !== "/login"
+    ) {
+      router.push("/login");
     }
-  }, [pathname, router])
-  
+  }, [pathname, router]);
+
   return (
     <SessionProvider>
       <GoogleOAuthProvider
         clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}
       >
         <Component {...pageProps} />
+        <ToastContainer />
       </GoogleOAuthProvider>
     </SessionProvider>
   );
