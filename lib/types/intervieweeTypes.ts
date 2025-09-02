38796b7d5ec0868e9505af12ae374152e55258dd
hsiao <10856003@ntub.edu.tw>
2025-09-02 19:00:08 +0800

    IntervieweeTypes

--- lib/types/intervieweeTypes.ts
+++ lib/types/intervieweeTypes.ts
@@ -1,18 +1,26 @@
 export type IntervieweeTypes = {
+    opening: string;
+    remark: string;
     interview_id: string;
     candidate_detail?: {
         username: string;
+        email: string;
+        phone_number: string;
     };
     opening_detail?: {
         opening_name: string;
         company_name: string;
     };
-    interview_status: string;
+    interview_status?: string;
     interview_datetime: string | number | Date;
     interview_result: string;
     total_score: number;
     interviewState?: boolean;
     interview_date?: string;
     interview_time?: string;
-    
+    score_expression?: number;
+    score_attitude?: number;
+    score_technical?: number;
+    score_collaboration?: number;
+    result_abstract?: string;
 }
