7118650a4d02f598e0bc5105a308743ccbf22454
hsiao <10856003@ntub.edu.tw>
2025-09-11 11:16:01 +0800

    admin/edit

---
+++ lib/data/admin/companyCreateData.ts
@@ -0,0 +1,10 @@
+import { InputFieldTypes } from "@/lib/types/inputFieldTypes";
+
+export const companyCreateData: InputFieldTypes[] = [
+  {
+    name: "opening_name",
+    label: "公司名稱",
+    type: "text",
+    placeholder: "請輸入公司名稱，例如：XX科技有限公司",
+  },
+];
