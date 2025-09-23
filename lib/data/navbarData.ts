d2b4cc0ec3c9b479718aa862af6d5f4b31188d64
hsiao <10856003@ntub.edu.tw>
2025-09-23 19:15:33 +0800

    admin nav

--- lib/data/navbarData.ts
+++ lib/data/navbarData.ts
@@ -1,17 +1,24 @@
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Business, LockOpen, Work } from "@mui/icons-material";
+import { Business, LockOpen } from "@mui/icons-material";
 
 const BASE_PATH = "/manage";
 
 export const NavbarData = [
   { name: "公司資訊", path: `${BASE_PATH}`, icon: Business },
   { name: "問題管理", path: `${BASE_PATH}/questions`, icon: HelpOutlineIcon },
-  // { name: "職位管理", path: `${BASE_PATH}/jobs`, icon: Work },
   { name: "職缺管理", path: `${BASE_PATH}/opening`, icon: LockOpen },
   {
     name: "面試者管理",
     path: `${BASE_PATH}/interviewee`,
     icon: AccountCircleIcon,
   },
+];
+
+const BASE_ADMIN_PATH = "/admin";
+
+export const AdminNavbarData = [
+  { name: "公司列表", path: `${BASE_ADMIN_PATH}/company`, icon: Business },
+  { name: "權限管理", path: `${BASE_ADMIN_PATH}/permissions`, icon: LockOpen },
+
 ];
